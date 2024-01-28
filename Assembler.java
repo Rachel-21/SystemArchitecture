44390ccd9c76514f46a6ddee90d8ec0d65fa9844
Rachel Gonzalez Rodriguez <61662217+Rachel-21@users.noreply.github.com>
2024-01-28 15:18:14 -0500

    in progress

---
+++ Assembler.java
@@ -0,0 +1,26 @@
+import java.io.*;
+
+
+public class Assembler 
+{
+    //need to read file 
+    public static void main(String[] args)
+    {
+
+        String path = "imput.txt";
+    }
+
+    //interpret file 
+    public static void interpret(String line)
+    {
+
+    }
+
+     //output file 
+    public static void outputFile(String out)
+    {
+
+    }
+   
+
+}
