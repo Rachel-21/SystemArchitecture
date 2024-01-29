1f3c2a89dc274d39a0522d3ae51dc0e6c8d150f2
Rachel Gonzalez Rodriguez <61662217+Rachel-21@users.noreply.github.com>
2024-01-28 19:03:15 -0500

    in progress

--- Assembler.java
+++ Assembler.java
@@ -1,26 +1,46 @@
 import java.io.*;
-
+import java.nio.charset.StandardCharsets;
+import java.util.*;
 
 public class Assembler 
 {
-    //need to read file 
-    public static void main(String[] args)
+    public static void main(String[] args) throws FileNotFoundException
     {
-
-        String path = "imput.txt";
+        System.out.println("Start Assembler reading");
+        String fileSource = "imput.txt";
+        Read(fileSource);
+    }
+    //need to read file 
+    public static void Read(String source) throws FileNotFoundException
+    {
+        //get the file 
+        String path = source;
+        //get the first line and start reading
+        InputStream line = new FileInputStream(path);
+ 
+        try (Scanner sc = new Scanner(line, StandardCharsets.UTF_8.name())) 
+        {
+            //reading the lines 
+            //print to check for errors
+            while (sc.hasNextLine()) 
+            {
+                //System.out.println(sc.nextLine());
+                interpret(sc.nextLine());
+            }
+        }
     }
 
     //interpret file 
     public static void interpret(String line)
     {
-
+        System.out.println(line);
     }
 
-     //output file 
+    /* //output file 
     public static void outputFile(String out)
     {
 
     }
-   
+   */
 
 }
